63952898e255908b0600a76f001e8c0599b26be2
Saba <sabanes048@gmail.com>
2024-06-04 14:18:32 +0100

    melhorado

--- src/components/ui/text-generate-effect.tsx
+++ src/components/ui/text-generate-effect.tsx
@@ -1,6 +1,6 @@
+"use client";
 import React, { useEffect, useRef } from "react";
-import { motion, useAnimation } from "framer-motion";
-import { useInView } from "react-intersection-observer";
+import { motion, stagger, useAnimate } from "framer-motion";
 import { cn } from "../utils/utils";
 
 export const TextGenerateEffect = ({
@@ -10,39 +10,68 @@ export const TextGenerateEffect = ({
   words: string;
   className?: string;
 }) => {
-  const controls = useAnimation();
-  const { ref, inView } = useInView({
-    triggerOnce: true,
-    threshold: 0.5, // Adjust threshold as per your requirement
-  });
-  const wordsArray = words.split(" ");
+  const [scope, animate] = useAnimate();
+  const containerRef = useRef(null);
+  let wordsArray = words.split(" ");
 
   useEffect(() => {
-    if (inView) {
-      controls.start("visible");
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            animate(
+              "span",
+              {
+                opacity: 1,
+              },
+              {
+                duration: 2,
+                delay: stagger(0.2),
+              }
+            );
+            observer.unobserve(entry.target);
+          }
+        });
+      },
+      {
+        threshold: 0.1,
+      }
+    );
+
+    if (containerRef.current) {
+      observer.observe(containerRef.current);
     }
-  }, [controls, inView]);
+
+    return () => {
+      if (containerRef.current) {
+        observer.unobserve(containerRef.current);
+      }
+    };
+  }, [animate]);
+
+  const renderWords = () => {
+    return (
+      <motion.div ref={scope}>
+        {wordsArray.map((word, idx) => (
+          <motion.span
+            key={word + idx}
+            className="opacity-0"
+          >
+            {word}{" "}
+          </motion.span>
+        ))}
+      </motion.div>
+    );
+  };
 
   return (
     <div className={cn("font-bold", className)}>
       <div className="mt-4">
-        <div className="md:text-3xl text-2xl leading-snug tracking-wide">
-          <motion.div ref={ref}>
-            {wordsArray.map((word, idx) => (
-              <motion.span
-                key={word + idx}
-                className="opacity-0"
-                variants={{
-                  visible: { opacity: 1, transition: { duration: 0.4, delay: idx * 0.2 } },
-                  hidden: { opacity: 0 },
-                }}
-                initial="hidden"
-                animate={controls}
-              >
-                {word}{" "}
-              </motion.span>
-            ))}
-          </motion.div>
+        <div
+          className="text-2xl leading-snug tracking-wide"
+          ref={containerRef}
+        >
+          {renderWords()}
         </div>
       </div>
     </div>
