ee1a7f092b0667d281b59f7e0d04360fef33a527
Saba <sabanes048@gmail.com>
2024-10-22 03:34:17 +0100

    not imp

--- src/components/ui/hero-parallax.tsx
+++ src/components/ui/hero-parallax.tsx
@@ -7,7 +7,7 @@ import {
   useSpring,
   MotionValue,
 } from "framer-motion";
-import Link from "next/link";
+
 
 export const HeroParallax = ({
   products,
@@ -137,7 +137,7 @@ export const ProductCard = ({
       key={product.title}
       className="group/product h-96 w-[30rem] relative flex-shrink-0"
     >
-      <Link
+      <a
         href={product.link}
         className="block group-hover/product:shadow-2xl "
       >
@@ -149,7 +149,7 @@ export const ProductCard = ({
           className="object-cover object-left-top absolute h-full w-full rounded-2xl inset-0"
           alt={product.title}
         />
-      </Link>
+      </a>
       <div className="absolute inset-0 h-full w-full opacity-0 group-hover/product:opacity-80 bg- pointer-events-none"></div>
       <h2 className="absolute bottom-4 left-4 opacity-0 group-hover/product:opacity-100 text-white">
         {product.title}
