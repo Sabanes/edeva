7fbb9b931fa3e3bb7e7e46d853283fab7d588c88
Saba <sabanes048@gmail.com>
2024-10-22 19:31:36 +0100

    cms improvemnt again 3

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,6 @@ import CMS from "decap-cms-app";
 // Initialize the CMS object
 CMS.init();
 // Now the registry is available via the CMS object.
-CMS.registerPreviewTemplate("my-template", MyTemplate);
 
 function App() {
 
