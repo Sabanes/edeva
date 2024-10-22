6cc8392a1d7109012f8ab0f656f6865bf97548e9
Saba <sabanes048@gmail.com>
2024-10-22 19:06:34 +0100

    path improved

--- public/admin/cms.js
+++ public/admin/cms.js
@@ -1,7 +1,7 @@
 // public/admin/cms.js
 
 import CMS from 'decap-cms-app'; // Import the Decap CMS library
-import MyTemplate from '../src/components/MyTemplate'; // Import the MyTemplate component
+import MyTemplate from '../../src/components/MyTemplate'; // Import the MyTemplate component
 
 // Register the preview template
 CMS.registerPreviewTemplate('my-template', MyTemplate);
